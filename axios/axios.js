d6c713b57db05e422ca6281fff9a352c4dd56a11
sachux <sachinkalpetta@gmail.com>
2022-03-15 23:56:34 +0530

    sample axios code

---
+++ axios/axios.js
@@ -0,0 +1,28 @@
+const axios = require("axios");
+const chalk = require("chalk");
+const log = console.log;
+
+const API_URL = "https://gorest.co.in/public/v2/users";
+
+/**
+ * This will fetch the conversion details from an external api
+ * @returns Promise
+ */
+const getConversionDetails = async () => {
+    try {
+        let res = await axios.get(API_URL);
+
+        if (res.status !== 200) {
+            log(chalk.red(`unable to get conversion details response status: ${res.status}`));
+            throw new Error(`unable to get conversion details response status: ${res.status}`);
+        } else {
+            log(chalk.blue(`Fetched ${res.data.length} conversion  rates from external API`));
+            return res.data;
+        }
+    } catch (e) {
+        throw new Error(`unable to get conversion details from external API: ${CONVERSION_API_URL}`, e);
+    }
+
+};
+
+log(getConversionDetails());
