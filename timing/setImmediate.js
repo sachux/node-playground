6048aa5b49fdb14ae0c1cd4da09f940c3a7bfda3
sachux <sachinkalpetta@gmail.com>
2022-03-16 00:55:15 +0530

    setImmediate vs others

--- timing/setImmediate.js
+++ timing/setImmediate.js
@@ -6,3 +6,18 @@ setImmediate(() => {
     console.log("third");
 });
 console.log("fourth");
+
+setImmediate(() => console.log('I run immediately'))
+
+process.nextTick(() => console.log('But I run before that'))
+
+// Output:
+// first
+// fourth
+// But I run before that
+// second
+// third
+// I run immediately
+
+// A nice blog on this topic :
+//https://jinoantony.com/blog/setimmediate-vs-process-nexttick-in-nodejs
