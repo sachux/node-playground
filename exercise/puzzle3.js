13f9d011726ae42960b66a02e9ff48d07e7c284b
sachux <sachinkalpetta@gmail.com>
2022-03-24 23:35:38 +0530

    Puzzle3

---
+++ exercise/puzzle3.js
@@ -0,0 +1,19 @@
+var x = 23;
+
+(function(){
+  var x = 43;
+  (function random(){
+    x++;
+    console.log(x);
+    var x = 21;
+  })();
+})();
+
+
+// Output is NaN .
+// function random(){
+//     var x; // x is hoisted
+//     x++; // x is not a number since it is not initialized yet
+//     console.log(x); // Outputs NaN
+//     x = 21; // Initialization of x
+//   }
